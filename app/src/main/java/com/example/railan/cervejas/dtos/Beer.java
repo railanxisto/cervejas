495ae510c0dfcb2827a9bd0bef5ffe609c0d4583
Railan Xisto Silva <railan.xisto@gmail.com>
2018-08-06 22:52:37 -0300

    Camada de Rede

---
+++ app/src/main/java/com/example/railan/cervejas/dtos/Beer.java
@@ -0,0 +1,49 @@
+package com.example.railan.cervejas.dtos;
+
+/**
+ * Created by railan on 06/08/18.
+ */
+
+public class Beer {
+    private int id;
+    private String name;
+    private String description;
+    private String image_url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    @Override
+    public String toString() {
+        return "id " + id + " nome: " + name;
+    }
+}
